88ca74ada50364577efcd55695e55c1887c25ef5
Mohammed ameen <ameennitw@gmail.com>
2024-07-27 15:58:49 +0530

    commiting changes of home page making alert

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,11 @@ import image4 from "../assets/coverimages/image4.jpg";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+// import alert1 from "../assets/Frame.svg"
+// import alert2 from "../assets/Frame1.svg"
+import alert1 from "../assets/alert1.svg"
+import alert2 from "../assets/alert2.svg"
+import NotificationBar from './NotificationBar';
 
 const Home = () => {
 
@@ -51,9 +56,32 @@ const Home = () => {
         </div>
 
 
-        <div className="alert-section w-full h-10 ">
+        <div className="alert-section w-full h-[20px] sm:h-[25px] md:h-[30px] lg:h-10 bg-[#0C223D] flex items-center ">
+          <div className='relative h-full'>
+
+            <img src={alert1} alt="" className='h-[100%] shadow-lg '/>
+            <span className=' absolute pl-2 h-full left-0 top-0 flex items-center font-bold'>
+              Alert
+            </span>
+
+          </div>
           
+          
+          <div className='  flex-grow h-full flex items-center  text-white'>  </div>
+
+          
+          <div className='relative h-full w-20'>
+
+            <img src={alert2} alt="" className='h-[100%] shadow-lg w-full object-cover'/>
+            <span className=' absolute pr-1 h-full right-0 top-0 flex items-center font-bold'>
+              <a href="">View All</a>
+            </span>
+
+          </div>
         </div>
+
+
+
       </div>
     </div>
   );
