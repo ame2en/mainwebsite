5f4779ba5a37c39468d797e8fb1063deb86ac9a5
Mohammed ameen <ameennitw@gmail.com>
2024-08-06 11:42:02 +0530

    commiting footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,68 @@ import React from 'react';
 import "./footer.css";
 
 
+
+
+
 const Footer = () => {
   return (
-    <div  className='w-full h-[100vh] footer  '>
+    <div  className='w-full mb-10  footer  '>
       <div className=' w-full h-full bg-gradient-to-tr from-[rgba(138,0,0,0.73)] to-[rgba(0,31,73,0.85)]'>
-        <div className='w-full h-full flex flex-wrap justify-around items-center py-12'>
-          {/* <div className="w-[20%] h-full bg-[rgba(0,0,0,0.5)]"></div>
-          <div className="w-[20%] h-full bg-slate-400"></div>
-          <div className="w-[20%] h-full bg-slate-400"></div>
-          <div className="w-[20%] h-full bg-slate-400"></div> */}
+        <div className='w-full h-full flex flex-wrap justify-around  py-12 text-[#C9C9C9]'>
+
+
+
+          <div className="w-[20%] h-full ">
+            <span className='font-semibold text-[1.5rem] text-white'>| <span className='pl-4 '>TENDERS</span></span>
+            <div className='mt-4 flex flex-col pl-2  font-semibold'>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>P&MM Tender</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Project Tenders</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Civil Works</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>O&M Tenders</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Erection of Works</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>General Tenders</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>E-Procument</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Vendor Registration</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Approved vendors List</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Blacklisted Vendors</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Blacklisted Vendors By Other Org</a></p>
+            </div>
+          </div>
+
+          <div className="w-[20%] h-full ">
+            <span className='font-semibold text-[1.5rem] text-white'>| <span className='pl-4 '>CONSUMERS</span></span>
+            <div className='mt-4 flex flex-col pl-2'>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>AGL Seniority List</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Consumer Login</a></p>
+            </div>
+          </div>
+          <div className="w-[20%] h-full ">
+            <span className='font-semibold text-[1.5rem] text-white'>| <span className='pl-4 '>IMPORTANT LINKS</span></span>
+            <div className='mt-4 flex flex-col pl-2'>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Official Mall</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Meeseva</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>TG-IPASS</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Usefull Links</a></p>
+            </div>
+          </div>
+          <div className="w-[20%] h-full ">
+            <span className='font-semibold text-[1.5rem] text-white'>| <span className='pl-4 '>OTHER LINKS</span></span>
+            <div className='mt-4 flex flex-col pl-2'>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Uday</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Open Access</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Post-Go Live Reports</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Outage Information - Urja Mitra</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Power Outages</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>SCADA Warangal Town</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>New Service Release Dashboard</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Electricity Duty Act,1939</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Approved Type Test Validity Guidelines</a></p>
+              <p className=' border-b-[1px] border-b-[rgba(160,160,160,0.36)] p-2 '><a href="" className=' hover:pl-2 hover:text-white duration-300 ease-in-out '>Circle Wise Losses</a></p>
+            </div>
+          </div>
+
+
+         
 
         </div>
 
